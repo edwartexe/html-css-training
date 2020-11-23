6374f4c0a7da73d4ac3b242e26f9c892fec47ca8
Eduardo Ramirez <eduardoramirez@MacBook-Pro-de-Eduardo.local>
2020-11-23 16:58:37 -0500

    folder structure

---
+++ assets/scripts/index.js
@@ -0,0 +1,125 @@
+function myFunction() {
+  let burger = document.getElementsByClassName("nav_menu-toggle")[0];
+  let burgerMenu = document.getElementsByClassName("nav_menu")[0];
+  let mainnav = document.getElementsByClassName("nav")[0];
+  burger.classList.toggle("open");
+  burgerMenu.classList.toggle("open");
+  mainnav.classList.toggle("closed");
+}
+
+$(document).ready(function(){
+  $('.brand_slider').slick({
+    variableWidth: true,
+    centerMode: true,
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    initialSlide:2,
+    responsive: [
+      {breakpoint: 1160,
+        settings: {
+          infinite: true,
+          mobileFirst:true,
+          pauseOnFocus:true,
+          arrows: false,
+          speed: 300,
+          slidesToShow: 1,
+          centerMode: true,
+          variableWidth: true,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        }
+      }
+    ]
+  });
+
+  $('.card-slider').slick({
+    variableWidth: true,
+    centerMode: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide:1,
+    responsive: [
+      {breakpoint: 1440,
+        settings: {
+          dots: true,
+          arrows: false,
+          infinite: true,
+          mobileFirst:true,
+          pauseOnFocus:true,
+          variableWidth: true,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        }
+      }
+    ]
+  });
+
+  $('.industry-slider').slick({
+    variableWidth: true,
+    centerMode: true,
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    initialSlide:2,
+    responsive: [
+      {breakpoint: 1440,
+        settings: {
+          infinite: true,
+          mobileFirst:true,
+          pauseOnFocus:true,
+          arrows: false,
+          variableWidth: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        }
+      }
+    ]
+  });
+
+  $('.partner-slider').slick({
+    variableWidth: true,
+    centerMode: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide:1,
+    responsive: [
+      {breakpoint: 833,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide:1,
+        }
+      }
+    ]
+  });
+
+});
+
+/*function deslickify(){
+$('.brand_slider').slick("unslick");
+
+$('.card-slider').slick("unslick");
+
+$('.industry-slider').slick("unslick");
+
+$('.partner-slider').slick("unslick");
+}
+
+
+slickify();
+
+$(window).resize(function(){
+var $windowWidth = $(document).width();
+if ($windowWidth < 1441) {
+    slickify();   
+}else{
+    $deslickify();
+}
+});*/
